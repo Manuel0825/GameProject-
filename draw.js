12ebf50fd88702ae94e9e02d09007db604104200
Manuel Vallejo <manuelvallejo@Manuels-MacBook-Pro.local>
2023-11-08 21:13:12 +0100

    Updated

--- draw.js
+++ draw.js
@@ -1,30 +1,66 @@
 /*frameRate(60); // Increase frame rate for smoother animation use for later on */
 
-let ballX = 450;
+/*let ballX = 450;
 let ballY = 300;
 let ballSpeedX = 0.2;
-let ballSpeedY= 4;
-
-
-
+let ballSpeedY = 4;
 
 function setup() {
-    createCanvas(900, 600); // Crea un lienzo de 900lx600h píxeles
+  createCanvas(900, 600); // Crea un lienzo de 900lx600h píxeles
+}
+
+function draw() {
+  background(0); // Establece el fondo negro
+  ellipse(ballX, ballY, 50, 50); // Dibuja un círculo en (450l, 300h) con un radio de 50
+
+  moveBall(); // CALL THE FUNCTION TO MOVE THE BALL
+  canvasBorders();
+  movePaddles();
+	p1.show();
+	p2.show();
+
+}*/
+
+let leftscore = 0;
+let rightscore = 0;
+
+function setup() {
+    createCanvas(1000, 500);
+    puck = new Puck();
+    left = new Paddle(true);
+    right = new Paddle(false);
+  
+}
+
+function draw() {
+    background (255, 204, 0);
     
+    puck.checkPaddleRight(right);
+    puck.checkPaddleLeft(left);
+
+    left.show();
+    right.show();
+    left.update();
+    right.update();
+    movePaddles();
+    
+    puck.update();
+    puck.edges();
+    puck.show();
+    
+    fill(255);
+    textSize(32);
+    text(leftscore, 32, 40);
+    text(rightscore, width-64, 40);
 }
 
 
 
-  
-  function draw() {
-    background(0); // Establece el fondo negro
-    ellipse(ballX, ballY, 50, 50); // Dibuja un círculo en (450l, 300h) con un radio de 50
 
-   
-    
-    moveBall();// CALL THE FUNCTION TO MOVE THE BALL 
-    canvasBorders();
-    Showpadel();
-  }
+
+
+
+
+      
 
 
--- padel.js
+++ padel.js
@@ -1,17 +1,72 @@
-let X;
-let Y ;
-let W = 100;
-let H = 25;
-let padelLeft;
-let padelRight;
+/*class Paddle {
 
+    constructor(x, y, w, h) {
+        this.pos = createVector(x, y);
+        this.w = w;
+        this.h = h;
+        this.score = 0;
+    }
 
+    move(amt) {
+        this.pos.y += amt;
+        this.pos.y = constrain(this.pos.y, 10, height - 10 - this.h);
+    }
 
-function Showpadel(){
-    fill(255);
-    rectMode(CENTER);
-    rect(X, Y, W, H);
+    show() {
+        noStroke();
+        fill(255);
+        rect(this.pos.x, this.pos.y, this.w, this.h);
+    }
+        
 
-  
+}*/
+
+class Paddle {
+    constructor(isLeft) {
+        this.y = height/2;
+        this.w = 20;
+        this.h = 100;
+        this.ychange = 0;
+        
+        if (isLeft) {
+            this.x = this.w;
+        } else {
+            this.x = width - this.w;
+        }
+        
+        
+    }
+    
+    update() {
+        this.y += this.ychange;
+        this.y = constrain(this.y, this.h/2, height-this.h/2);
+    }
+    
+    move(steps) {
+        this.ychange = steps;
+    }
+    
+    show() {
+        fill(255);
+        rectMode(CENTER);
+        rect(this.x, this.y, this.w, this.h);
+    }
 }
+
+
+
+function movePaddles() {
+    if (keyIsDown(65)) { // 65 is the key code for 'A'
+        left.move(-5);
+    } else if (keyIsDown(90)) { // 90 is the key code for 'Z'
+        left.move(5);
+    }
+
+    if (keyIsDown(75)) { // 75 is the key code for 'J'
+        right.move(-5);
+    } else if (keyIsDown(77)) { // 77 is the key code for 'M'
+        right.move(5);
+    }
+}
+
 
